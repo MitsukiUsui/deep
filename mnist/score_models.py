853fa90bf229cff6018f016e83105adf21fa34fe
MitsukiUsui <mitsuki@Mitsuki.deep>
2017-06-29 18:02:11 +0900

    score_models.py

---
+++ mnist/score_models.py
@@ -0,0 +1,10 @@
+import score
+from mymodels import knn
+from mymodels import mlp_notf
+from mymodels import mlp
+from mymodels import cnn
+
+score.score(knn.knn, k_max=10)
+score.score(mlp_notf.mlp_notf, batch_size=50)
+score.score(mlp.mlp, batch_size=50)
+score.score(cnn.cnn)
