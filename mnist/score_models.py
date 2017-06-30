1db5bafe62f1d9b1c2511d4ae9b59b6ab701e4e6
MitsukiUsui <mitsuki@Mitsuki.deep>
2017-06-30 10:40:53 +0900

    confirmed working on GPU server

--- mnist/score_models.py
+++ mnist/score_models.py
@@ -4,7 +4,7 @@ from mymodels import mlp_notf
 from mymodels import mlp
 from mymodels import cnn
 
-score.score(knn.knn, k_max=10)
-score.score(mlp_notf.mlp_notf, batch_size=50)
-score.score(mlp.mlp, batch_size=50)
+#score.score(knn.knn, k_max=10)
+#score.score(mlp_notf.mlp_notf, batch_size=50)
+#score.score(mlp.mlp, batch_size=50)
 score.score(cnn.cnn)
